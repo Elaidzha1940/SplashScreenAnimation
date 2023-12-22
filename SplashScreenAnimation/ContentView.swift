e79089d9a8bee7c900b8aece49ed0ab5ec3130eb
Elaidzha1940 <eli080293@gmail.com>
2023-12-22 14:34:48 +0300

    SplashScreenAnimation

---
+++ SplashScreenAnimation/ContentView.swift
@@ -0,0 +1,23 @@
+//  /*
+//
+//  Project: SplashScreenAnimation
+//  File: ContentView.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.12.2023
+//
+//  */
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SplashScreenAnimation/SplashScreenAnimationApp.swift
@@ -0,0 +1,19 @@
+//  /*
+//
+//  Project: SplashScreenAnimation
+//  File: SplashScreenAnimationApp.swift
+//  Created by: Elaidzha Shchukin
+//  Date: 22.12.2023
+//
+//  */
+
+import SwiftUI
+
+@main
+struct SplashScreenAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
